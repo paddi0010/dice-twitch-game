7d49d5e5bbeed5539d27937ffef293d5bf6503fd
paddi0010 <kontakt.paddi001@gmail.com>
2023-12-04 23:55:14 +0100

    -

--- dice_game.js
+++ dice_game.js
@@ -15,29 +15,45 @@ const client = new tmi.Client({
 
 const winNumber = Math.floor(Math.random() * 100) + 1;
 let diceActive = false;
-let attemps = 7;
+let attempts = 7;
+let lastGameTime = 0;
+const cooldownDuration = 60000; // 1 minute cooldown
 
 client.on('message', (channel, userstate, message, self) => {
     if (self) return;
 
     if (message.toLowerCase() === '!dice start' && !diceActive) {
-        diceActive = true;
-        trys = 5;
-        client.say(channel, 'Dice has been begun! Type a Number between 1 and 100 in the Chat! You have 5 Attemps!');
+        const currentTime = Date.now();
+
+        if (currentTime - lastGameTime >= cooldownDuration) {
+            lastGameTime = currentTime;
+            attempts = 5;
+            diceActive = true;
+            client.say(channel, 'Dice has been begun! Type a Number between 1 and 100 in the Chat! You have 5 Attempts!');
+        } else {
+            const remainingCooldown = Math.ceil((cooldownDuration - (currentTime - lastGameTime)) / 1000);
+            client.say(channel, `Please wait ${remainingCooldown} seconds before starting a new game.`);
+        }
+    }
+
+    if (message.toLowerCase() === '!dice stop' && diceActive) {
+        client.say(channel, 'Dice game has been stopped.');
+        diceActive = false;
+        attempts = 7;
     }
 
     if (diceActive) {
         const type = parseInt(message);
 
-        if (!isNaN(type) && type >= 1 && type <= 100) {
-            if (type == winNumber) {
-                client.say(channel, `${userstate['display-name']} has win the Dice! The Number was: ${winNumber}.`);
+        if (!isNaN(type) && Number.isInteger(type) && type >= 1 && type <= 100) {
+            if (type === winNumber) {
+                client.say(channel, `${userstate['display-name']} has won the Dice! The Number was: ${winNumber}.`);
                 diceActive = false;
             } else {
                 const hint = type < winNumber ? 'Higher' : 'Lower';
-                client.say(channel, `${userstate['display-name']}, ${hint} You have ${--attemps} Attemps.`);
+                client.say(channel, `${userstate['display-name']}, ${hint}. You have ${--attempts} attempts remaining.`);
 
-                if (attemps === 0) {
+                if (attempts === 0) {
                     client.say(channel, `You have spent all the attempts! The Number was ${winNumber}.`);
                     diceActive = false;
                 }
@@ -45,6 +61,5 @@ client.on('message', (channel, userstate, message, self) => {
         }
     }
 });
-
 
 client.connect().catch(console.error);
